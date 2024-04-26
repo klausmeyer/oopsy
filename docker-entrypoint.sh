c593cafebebed6b98234482d24af308003b3ebd5
Klaus Meyer <spam@klaus-meyer.net>
2024-04-26 13:56:14 +0200

    update ruby setup (#227)

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -5,10 +5,14 @@ set -e
 case "$1" in
 
   web)
+    bundle exec rails db:prepare
+
     exec bundle exec puma -C config/puma.rb
     ;;
 
   sidekiq)
+    bundle exec rails db:prepare
+
     shift
     exec bundle exec sidekiq $@
     ;;
