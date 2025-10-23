11a406e67b9c85c748e154ca0c30e8b9b2273200
Klaus Meyer <spam@klaus-meyer.net>
2025-10-23 09:29:32 +0200

    More

--- config/ci.rb
+++ config/ci.rb
@@ -6,7 +6,7 @@ CI.run do
   step "Style: Ruby", "bin/rubocop"
 
   # step "Security: Importmap vulnerability audit", "bin/importmap audit"
-  step "Tests: Rails", "bin/rails test"
+  step "Tests: Rails", "bin/rails spec"
   step "Tests: Seeds", "env RAILS_ENV=test bin/rails db:seed:replant"
 
   # Optional: set a green GitHub commit status to unblock PR merge.
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -25,10 +25,10 @@ Rails.application.configure do
   config.active_storage.service = :local
 
   # Assume all access to the app is happening through a SSL-terminating reverse proxy.
-  config.assume_ssl = false
+  config.assume_ssl = ENV["ASSUME_SSL"].in? %w[true 1 yes]
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
-  config.force_ssl = false
+  config.force_ssl = ENV["FORCE_SSL"].in? %w[true 1 yes]
 
   # Skip http-to-https redirect for the default health check endpoint.
   # config.ssl_options = { redirect: { exclude: ->(request) { request.path == "/up" } } }
